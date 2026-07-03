ec6f4abc2344769557b360b6e567cbf0aa9bebc1
C-Dub1978 <chris@cswilson.site>
2026-07-03 18:33:13 -0500

    last fix for the deploy

--- theme-designer/src/environments/environment.ts
+++ theme-designer/src/environments/environment.ts
@@ -1,6 +1,5 @@
-
-    export const environment = {
-        production: true,
-        apiUrl: 'https://cswilson.site',
-        primeNgKey: 'YOUR_PRODUCTION_GITHUB_SECRET_PLACEHOLDER'
-    };
+export const environment = {
+    production: true,
+    apiUrl: 'https://cswilson.site',
+    primeNgKey: import.meta.env["NG_APP_PRIME_NG_KEY"] // Reads from GitHub runner memory
+  };
